1f9398315d8ecc2700ce9efd62f25de5aaafd36e
breadthe <omigoshdev@protonmail.com>
2023-10-01 16:54:38 -0500

    404 tag not found page

---
+++ src/routes/blog/tags/[[tag]]/+error.svelte
@@ -0,0 +1,9 @@
+<script>
+    import { page } from '$app/stores';
+</script>
+
+<div class="m-8 p-8 flex flex-col gap-4">
+    <h1 class="text-4xl pb-2 font-bold border-b border-[--border-color]">{$page.status}</h1>
+    <p class="text-2xl text-[--text-color-muted]">{@html $page.error.message}</p>
+    <p class="mt-8">Check out the <a href="/blog/tags">tags index</a> or <a href="/blog">blog index</a> instead.</p>
+</div>
--- src/routes/blog/tags/[[tag]]/+page.js
+++ src/routes/blog/tags/[[tag]]/+page.js
@@ -1,3 +1,5 @@
+import { error } from '@sveltejs/kit'
+
 export const load = async ({ fetch, params }) => {
     const { tag } = params
     let serverUrl = `/api/tags`
@@ -6,6 +8,10 @@ export const load = async ({ fetch, params }) => {
 
     const response = await fetch(serverUrl)
     const tags = await response.json()
+
+    if (tag && Object.keys(tags).length === 0) {
+        throw error(404, `Tag <strong>${tag}</strong> not found`)
+    }
 
     return {
         tags,
